c8fe55cebc6ef00fccf4138e6eaf04ee4bc56068
Arvind Gill <arvindkhtmuz@gmail.com>
2022-08-23 11:24:16 +0530

    [arvind] Add: folder structure created

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeWage employeeWage = new EmployeeWage();
+            employeeWage.Attendence();
+        }
+    }
+}
